527c64eeaf7c53d79cbd90a6fef80eaaa6164823
paolino <paolo.veronelli@gmail.com>
2026-02-13 18:16:19 +0000

    feat: display full CIP-30 wallet info after connecting
    Query and display network ID, balance, UTXO count, change address,
    used/unused/reward addresses from the Eternl CIP-30 API.

--- src/FFI/Wallet.js
+++ src/FFI/Wallet.js
@@ -12,3 +12,45 @@ export const signTxImpl = (api) => (cborHex) => (onError) => (onSuccess) => () =
     .then((witness) => onSuccess(witness)())
     .catch((err) => onError(new Error(String(err)))());
 };
+
+export const getNetworkIdImpl = (api) => (onError) => (onSuccess) => () => {
+  api.getNetworkId()
+    .then((id) => onSuccess(id)())
+    .catch((err) => onError(new Error(String(err)))());
+};
+
+export const getBalanceImpl = (api) => (onError) => (onSuccess) => () => {
+  api.getBalance()
+    .then((bal) => onSuccess(bal)())
+    .catch((err) => onError(new Error(String(err)))());
+};
+
+export const getUtxosImpl = (api) => (onError) => (onSuccess) => () => {
+  api.getUtxos()
+    .then((utxos) => onSuccess(utxos || [])())
+    .catch((err) => onError(new Error(String(err)))());
+};
+
+export const getUsedAddressesImpl = (api) => (onError) => (onSuccess) => () => {
+  api.getUsedAddresses()
+    .then((addrs) => onSuccess(addrs || [])())
+    .catch((err) => onError(new Error(String(err)))());
+};
+
+export const getUnusedAddressesImpl = (api) => (onError) => (onSuccess) => () => {
+  api.getUnusedAddresses()
+    .then((addrs) => onSuccess(addrs || [])())
+    .catch((err) => onError(new Error(String(err)))());
+};
+
+export const getChangeAddressImpl = (api) => (onError) => (onSuccess) => () => {
+  api.getChangeAddress()
+    .then((addr) => onSuccess(addr)())
+    .catch((err) => onError(new Error(String(err)))());
+};
+
+export const getRewardAddressesImpl = (api) => (onError) => (onSuccess) => () => {
+  api.getRewardAddresses()
+    .then((addrs) => onSuccess(addrs || [])())
+    .catch((err) => onError(new Error(String(err)))());
+};
--- src/FFI/Wallet.purs
+++ src/FFI/Wallet.purs
@@ -4,6 +4,13 @@ module FFI.Wallet
   , detectEternl
   , enableWallet
   , signTx
+  , getNetworkId
+  , getBalance
+  , getUtxos
+  , getUsedAddresses
+  , getUnusedAddresses
+  , getChangeAddress
+  , getRewardAddresses
   ) where
 
 import Prelude
@@ -31,6 +38,48 @@ foreign import signTxImpl
   -> (String -> Effect Unit)
   -> Effect Unit
 
+foreign import getNetworkIdImpl
+  :: WalletApi
+  -> (Error -> Effect Unit)
+  -> (Int -> Effect Unit)
+  -> Effect Unit
+
+foreign import getBalanceImpl
+  :: WalletApi
+  -> (Error -> Effect Unit)
+  -> (String -> Effect Unit)
+  -> Effect Unit
+
+foreign import getUtxosImpl
+  :: WalletApi
+  -> (Error -> Effect Unit)
+  -> (Array String -> Effect Unit)
+  -> Effect Unit
+
+foreign import getUsedAddressesImpl
+  :: WalletApi
+  -> (Error -> Effect Unit)
+  -> (Array String -> Effect Unit)
+  -> Effect Unit
+
+foreign import getUnusedAddressesImpl
+  :: WalletApi
+  -> (Error -> Effect Unit)
+  -> (Array String -> Effect Unit)
+  -> Effect Unit
+
+foreign import getChangeAddressImpl
+  :: WalletApi
+  -> (Error -> Effect Unit)
+  -> (String -> Effect Unit)
+  -> Effect Unit
+
+foreign import getRewardAddressesImpl
+  :: WalletApi
+  -> (Error -> Effect Unit)
+  -> (Array String -> Effect Unit)
+  -> Effect Unit
+
 -- | Enable the Eternl wallet and obtain an API handle.
 enableWallet :: Aff WalletApi
 enableWallet = makeAff \cb -> do
@@ -45,4 +94,60 @@ signTx api cbor = makeAff \cb -> do
   signTxImpl api cbor
     (\err -> cb (Left err))
     (\wit -> cb (Right wit))
+  pure mempty
+
+-- | Get the network ID (0 = testnet, 1 = mainnet).
+getNetworkId :: WalletApi -> Aff Int
+getNetworkId api = makeAff \cb -> do
+  getNetworkIdImpl api
+    (\err -> cb (Left err))
+    (\nid -> cb (Right nid))
+  pure mempty
+
+-- | Get the wallet balance as CBOR hex.
+getBalance :: WalletApi -> Aff String
+getBalance api = makeAff \cb -> do
+  getBalanceImpl api
+    (\err -> cb (Left err))
+    (\bal -> cb (Right bal))
+  pure mempty
+
+-- | Get all UTXOs as CBOR hex strings.
+getUtxos :: WalletApi -> Aff (Array String)
+getUtxos api = makeAff \cb -> do
+  getUtxosImpl api
+    (\err -> cb (Left err))
+    (\us -> cb (Right us))
+  pure mempty
+
+-- | Get all used addresses as hex strings.
+getUsedAddresses :: WalletApi -> Aff (Array String)
+getUsedAddresses api = makeAff \cb -> do
+  getUsedAddressesImpl api
+    (\err -> cb (Left err))
+    (\as -> cb (Right as))
+  pure mempty
+
+-- | Get all unused addresses as hex strings.
+getUnusedAddresses :: WalletApi -> Aff (Array String)
+getUnusedAddresses api = makeAff \cb -> do
+  getUnusedAddressesImpl api
+    (\err -> cb (Left err))
+    (\as -> cb (Right as))
+  pure mempty
+
+-- | Get the change address as hex string.
+getChangeAddress :: WalletApi -> Aff String
+getChangeAddress api = makeAff \cb -> do
+  getChangeAddressImpl api
+    (\err -> cb (Left err))
+    (\a -> cb (Right a))
+  pure mempty
+
+-- | Get reward/stake addresses as hex strings.
+getRewardAddresses :: WalletApi -> Aff (Array String)
+getRewardAddresses api = makeAff \cb -> do
+  getRewardAddressesImpl api
+    (\err -> cb (Left err))
+    (\as -> cb (Right as))
   pure mempty
